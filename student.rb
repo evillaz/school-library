0306df7f3041fd936453e14e24e09142c57f1771
evillaz <evillaz123@gmail.com>
2023-06-01 00:20:21 -0500

    Create classroom class student association

--- student.rb
+++ student.rb
@@ -3,9 +3,14 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name, parent_permission: true)
-    super(age, parent_permission: parent_permission, name: name)
+  def initialize(age, name, parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @classroom = nil
+  end
+
+  def classroom=(classroom)
     @classroom = classroom
+    @classroom.students.push(self) unless @classroom.students.include?(self)
   end
 
   def play_hooky
