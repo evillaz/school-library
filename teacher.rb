c9076668bb160d70c8755672abb0b9640ec5f5be
evillaz <evillaz123@gmail.com>
2023-05-31 21:01:02 -0500

    refactor to add name parameter

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < Person
   attr_reader :specialization
 
-  def initialize(age, specialization, parent_permission: true, name: 'Unknown')
+  def initialize(age, classroom, name, parent_permission: true)
     super(age, parent_permission: parent_permission, name: name)
     @specialization = specialization
   end
@@ -12,3 +12,5 @@ class Teacher < Person
     true
   end
 end
+
+te = Teacher.new(20, ' 2asd', 'asd')
