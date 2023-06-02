dc76ce64b3c2bfc71952c7a7cc7d1cc738092cac
evillaz <evillaz123@gmail.com>
2023-06-02 04:58:47 -0500

    Refactor to requirements

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name, parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  def initialize(age, name, parent_permission)
+    super(age, name, parent_permission)
     @classroom = nil
   end
 
--- teacher.rb
+++ teacher.rb
@@ -3,9 +3,9 @@ require_relative 'person'
 class Teacher < Person
   attr_reader :specialization
 
-  def initialize(age, name, parent_permission: true)
+  def initialize(age, name, specialization, parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @specialization = nil
+    @specialization = specialization
   end
 
   def can_use_services?
