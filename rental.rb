17f24ae768905674e7b4bdeb2318d3c27a4c08e4
evillaz <evillaz123@gmail.com>
2023-06-08 23:12:00 -0500

    Update rental.rb

--- rental.rb
+++ rental.rb
@@ -1,7 +1,7 @@
 class Rental
   attr_accessor :date, :book, :person
 
-  def initialize(date, book = nil, person = nil)
+  def initialize(date, book, person)
     @date = date
     @book = book
     @book.rentals.push(self) if !@book.nil? && !@book.rentals.include?(self)
