2bb5c95822770baac7d5c667d419062b862f9b4b
evillaz <evillaz123@gmail.com>
2023-06-10 03:15:15 -0500

    Add fizzbuzz method

--- TDD/solver.rb
+++ TDD/solver.rb
@@ -19,4 +19,16 @@ class Solver
     end
     reverse_word
   end
+
+  def fizzbuzz(n)
+    if n % 3 == 0 && n % 5 == 0
+      return 'fizzbuzz'
+    elsif n % 3 == 0
+      return 'fizz'
+    elsif n % 5 == 0
+      return 'buzz'
+    else 
+      return n.to_s
+    end
+  end
 end
