d8412fe369fca9cedd821725faa73ca150723a89
evillaz <evillaz123@gmail.com>
2023-06-10 03:18:58 -0500

    Correct linters

--- TDD/solver.rb
+++ TDD/solver.rb
@@ -1,13 +1,14 @@
 class Solver
-  def factorial(n)
-    raise ArgumentError, "Factorial input must be a non-negative integer" if n.negative?
-    return 1 if n.zero?
+  def factorial(num)
+    raise ArgumentError, 'Factorial input must be a non-negative integer' if num.negative?
+    return 1 if num.zero?
+
     result = 1
-    while (n > 1)
-      result *= n
-      n -= 1
+    while num > 1
+      result *= num
+      num -= 1
     end
-    return result
+    result
   end
 
   def reverse(word)
@@ -20,15 +21,15 @@ class Solver
     reverse_word
   end
 
-  def fizzbuzz(n)
-    if n % 3 == 0 && n % 5 == 0
-      return 'fizzbuzz'
-    elsif n % 3 == 0
-      return 'fizz'
-    elsif n % 5 == 0
-      return 'buzz'
-    else 
-      return n.to_s
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
     end
   end
 end
